c0d4d2e649a243165bc92613735bc052aa78bbaf
robmolloy <robmolloy@hotmail.co.uk>
2025-08-15 08:33:15 +0100

    yalc setup complete

--- src/components/MyButton.tsx
+++ src/components/MyButton.tsx
@@ -3,5 +3,5 @@ export interface MyButtonProps {
 }
 
 export const MyButton: React.FC<MyButtonProps> = (p) => {
-  return <div style={{ backgroundColor: "blue" }}>MyButton & {p.children}</div>;
+  return <div style={{ backgroundColor: "red" }}>MyButton & {p.children}</div>;
 };
