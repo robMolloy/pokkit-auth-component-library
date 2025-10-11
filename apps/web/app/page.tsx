0605c5e3a69920557802d68b98722f505e40a2cc
robmolloy <robmolloy@hotmail.co.uk>
2025-10-11 19:36:05 +0100

    switch to shadcn turborepo

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,6 +1,36 @@
-import { add } from "@repo/math";
-import styles from "./page.module.css";
+import { Button } from "@workspace/ui/components/button";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@workspace/ui/components/select";
 
-export default function Home() {
-  return <div className={styles.page}>{add(1, 2)}</div>;
+export default function Page() {
+  return (
+    <div className="flex items-center justify-center min-h-svh">
+      <div className="flex flex-col items-center justify-center gap-4">
+        <h1 className="text-2xl font-bold">Hello World</h1>
+        <Button size="sm">Button</Button>
+        <Select>
+          <SelectTrigger className="w-[180px]">
+            <SelectValue placeholder="Select a fruit" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectGroup>
+              <SelectLabel>Fruits</SelectLabel>
+              <SelectItem value="apple">Apple</SelectItem>
+              <SelectItem value="banana">Banana</SelectItem>
+              <SelectItem value="blueberry">Blueberry</SelectItem>
+              <SelectItem value="grapes">Grapes</SelectItem>
+              <SelectItem value="pineapple">Pineapple</SelectItem>
+            </SelectGroup>
+          </SelectContent>
+        </Select>
+      </div>
+    </div>
+  );
 }
--- packages/math/src/index.ts
+++
@@ -1,2 +0,0 @@
-export * from "./add.js";
-export * from "./subtract.js";
